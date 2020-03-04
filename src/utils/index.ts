a535e8a4f7df15549388cdddb5f95b21b92f1ec7
Michele Riva <ciao@micheleriva.it>
2020-03-04 18:16:03 +0100

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@
  * @returns {T} any
  * @description Trampoline function.
  * Transofrms recursion into a loop under the hood, so program won't excede
- * maximum stack call. Reference: https://medium.com/openmindonline/js-monday-06-adopting-memory-safe-recursion-d26dcee409c9
+ * maximum stack call. Reference: https://www.hackdoor.io/articles/5kQdVNDX/adopting-memory-safe-recursion
  */
 
 export function trampoline<T>(func: Function): T {
